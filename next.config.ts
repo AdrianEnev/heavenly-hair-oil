086ff2551575d4a47707e2993423c0ebb367497b
Adrian <enevadrian@gmail.com>
2026-03-05 13:43:27 +0200

    fix contact page issue for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    SES_SMTP_HOST: process.env.SES_SMTP_HOST,
+    SES_SMTP_PORT: process.env.SES_SMTP_PORT,
+    SES_SMTP_USER: process.env.SES_SMTP_USER,
+    SES_SMTP_PASS: process.env.SES_SMTP_PASS,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    EMAIL_FROM_NAME: process.env.EMAIL_FROM_NAME,
+    EMAIL_SUPPORT: process.env.EMAIL_SUPPORT,
+  },
 };
 
 export default nextConfig;
